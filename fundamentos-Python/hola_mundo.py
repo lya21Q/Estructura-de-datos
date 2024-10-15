7915fbc42214c59d33cf510cf4e709a852190d17
Admin <perezvelayuo@gmail.com>
2024-10-14 20:18:36 -0700

    se agregaron comentarios y se hicieron modificaciones

--- fundamentos-Python/hola_mundo.py
+++ fundamentos-Python/hola_mundo.py
@@ -1,1 +1,9 @@
+#Rosalinda Aquino Pérez
+# 14 oct del 2024
+# Este programa imprime el letrero " Hola Mundo 2
+
 print("hola mundo")
+#Nota :
+# En Python no es necesario agregar las librerias al incio del programa
+# Tampoco fue necesario la estructura de main, para poder correr el programa
+# Para la sintaxis pude notar el uso de parentesis para imprimir el letrero y no es necesario terminar la linea en punto y coma.
