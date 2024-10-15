88750eaff67a5ef70b984ef9a459aa147db24f18
Admin <perezvelayuo@gmail.com>
2024-10-14 20:02:38 -0700

    se agregaron comentarios y se hicieron modificaciones

---
+++ fundamentos-Python/Casting_ejercicio.py
@@ -0,0 +1,9 @@
+# Rosalinda Aquino Pérez
+# 14 de oct del 2024
+# Este programa imprime un  letrero: "hola mundo"
+
+print("Hola Mundo")
+#Notas :
+# En Python no es necesario declarar las librerias al principio del programa
+# Tambien hay cambios como el parentesis para imprimir el letrero y no finaliza la linea con el punto y coma
+# Tampoco es necesario usar la estructura del main para el programa
