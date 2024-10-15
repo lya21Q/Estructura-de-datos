ec0b4c5b1b31bbfdc9238a172fc70db38847c212
Admin <perezvelayuo@gmail.com>
2024-10-14 20:19:17 -0700

    se agregaron comentarios y se hicieron modificaciones

--- fundamentos-Python/hola_mundo.py
+++ fundamentos-Python/hola_mundo.py
@@ -6,4 +6,4 @@ print("hola mundo")
 #Nota :
 # En Python no es necesario agregar las librerias al incio del programa
 # Tampoco fue necesario la estructura de main, para poder correr el programa
-# Para la sintaxis pude notar el uso de parentesis para imprimir el letrero y no es necesario terminar la linea en punto y coma.
+# Para la sintaxis pude notar el uso de parentesis para imprimir el letrero y no es necesario terminar la linea en punto y coma
