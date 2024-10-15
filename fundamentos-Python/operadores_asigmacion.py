a7a0cc4b8e3705b64c31ed2bed40a34b28995030
rosalinda <perezvelayuo@gmail.com>
2024-10-15 16:09:27 -0600

    como hacer

---
+++ fundamentos-Python/operadores_asigmacion.py
@@ -0,0 +1,25 @@
+
+
+v1,v2=5,10
+print(v1,v2)
+v3,v4 = 9.14,"chuy"
+print(v3,v4)
+v5,v6=v1+v2, v1-v2
+print(v5,v6)
+
+#asignacion a cadena
+v7=v8=v9=10
+print(v7,v8,v9)
+v10,v11="Alberto", "geto"
+print(v10,v11)
+
+#intercambio
+v11,v10=v10,v11
+print(v10,v11)
+
+nombre=input("ingresa nombre :")
+print()
+apellido=input("ingresa apellido :")
+print()
+
+nombre,apellido=input("ingresa nombre"),input("ingresa apellido :")
---
+++ operadores-en-python/operadores-aridmeticos.py
@@ -0,0 +1,11 @@
+#Rosalinda Aquino Pérez
+
+usuario = input("Ingrese su numero: ")
+
+usuario2 = input("Ingrese segundo numero: ")
+
+usuario11 = int(usuario)
+usuario23 = int(usuario2)
+print(usuario11 // usuario23)
+print(usuario11 ** usuario23)
+10
