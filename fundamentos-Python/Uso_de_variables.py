701d399832175ad917644dc4b41ec089e2c17fe0
Admin <perezvelayuo@gmail.com>
2024-10-15 00:21:34 -0700

    se agregaron comentarios y se hicieron modificaciones

--- fundamentos-Python/Uso_de_variables.py
+++ fundamentos-Python/Uso_de_variables.py
@@ -1,30 +1,27 @@
-# Alberto Martínez Barbosa
-# 7 de septiembre de 2024
+# Rosalinda Aquino Pérez
+# 14 de octubre  2024
 # En este archivo se ejemplifica el uso de variables en Python.
-
 # Notas:
-# En Python todo es un objeto.
 # Variable - una variable es un nombre que almacena un valor guardado en la memoria temporal.
 
 # Toda variable requiere un valor inicial
-semestre = 3        # es una variable que apunta a un objeto de tipo int con valor de 3
-print(semestre)     # imprime el valor de la variable
-semestre = 4        # la variable ya no apunta al objeto anterior, sino a uno nuevo, por lo que se pierde la referencia
+semestre = 3        # es una variable que almacena un valor de tipo entero
+print(semestre)     # imprime el valor de la variable semestre
+semestre = 4        # Se modificó el valor de la variable
 print(semestre)
 
-# Se crean varias variables para ejemplificar su uso
-nombre = "Alberto"  # variable de tipo String
-altura = 1.65       # variable de tipo Float
-edad = 31           # variable de tipo Int
-# Se imprimen las variables, añadiendo información adicional para comprender lo que se imprime
+nombre = "Rosalinda"  # variable de tipo String
+altura = 1.50       # variable de tipo Float
+edad = 20           # variable de tipo Int
+# Se modificó el valor de la variable
 print("Nombre:", nombre)
 print("Semestre:", semestre)
 print("Altura: ", altura, "cm.")
 print("Edad: ", edad, "años.")
 
-# Se modifican los valores de las variables y se mandan a imprimir
-altura = 1.66
-edad = 30
+# Se modifican los valores de las variables y se imprime
+altura = 1.60
+edad = 22
 print()
 print("Valores modificados:")
 print("Nombre:", nombre)
@@ -33,35 +30,24 @@ print("Altura: ", altura, "cm.")
 print("Edad: ", edad, "años.")
 
 # En Python, las variables son dinámicas, por lo que pueden almacenar otro tipo de dato en cualquier momento
-edad = "treinta y uno"      # edad antes tenía el valor de 31 (Int)
+edad = "treinta y uno" #Se cambió el contenido antes tenia el valor de un entero
 print()
 print("Edad (con otro tipo de dato):", edad)
-
-# Reglas para los nombres de las variables en Python:
-# - Utilizar únicamente letras (mayúsculas o minúsculas), números y el guion bajo
-# - La variable no puede iniciar con números
-# - No se pueden utilizar palabras reservadas, ejemplos: if, else, True, class
-# - Es sensible a mayúsculas y minúsculas. Por ejemplo, las palabras "Hola", "hola" y "HOLA" son consideradas diferentes.
-
-# Buenas prácticas
-# - Utilizar minúsculas para las palabras
-# - Separar las palabras con el guion bajo
-# - Utilizar nombres descriptivos de acuerdo a su uso. Por ejemplo: edad, en lugar de e.
 
 # Ejemplos correctos y con buenas prácticas
 fecha_nacimiento = "1 de enero del 2000"
 clase = "Estructuras de Datos"
 horas_estudio = 8
-nombre = "Alberto"
+nombre = "Rosalinda"
 es_estudiante = True
 
 # Ejemplos incorrectos (líneas comentadas porque marcan error) o de malas prácticas
-f = "1 de enero del 2000"
-fechanacimiento = "1 de enero del 2000"
+f = "31 de diciembre del 2003"
+fechanacimiento = "31 de diciembre del 2003"
 # class = "Estructuras de Datos"
 # 8horas_estudio = 8
-Nombre = "A l b e r t o"
-NOMBRE = "ALBERTO"
+Nombre = "R o s a l i n d a"
+NOMBRE = "ROSALINDA"
 
 # Notar que las variables 'nombre', 'Nombre' y 'NOMBRE', son distintas
 print()
