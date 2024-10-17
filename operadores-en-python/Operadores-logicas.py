17eba843e815834c6da36d942487b5cf3bf9f3e7
rosalinda <perezvelayuo@gmail.com>
2024-10-17 15:29:44 -0600

    como hacer

--- operadores-en-python/Operadores-logicas.py
+++ operadores-en-python/Operadores-logicas.py
@@ -4,3 +4,8 @@ expresion1= expresion1.lower()=="si"
 print(expresion1)
 expresion2= expresion2.lower()=="si"
 print(expresion2)
+
+print("f¿Ambas son verdaderas?: ",{expresion1 and expresion2})
+print("f¿Ambas son verdaderas?: ",{expresion1 or expresion2})
+print("f¿Ambas son verdaderas?: ",{not expresion1 })
+print("f¿Ambas son verdaderas?: ",{not expresion2 })
