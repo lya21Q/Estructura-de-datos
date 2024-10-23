5f7d6416ef479e5eb31ff279db9fa35255f92b26
Admin <perezvelayuo@gmail.com>
2024-10-22 20:45:06 -0700

    se realizaron comentarios

--- Entrada_conversiones_ejercicio.py
+++ Entrada_conversiones_ejercicio.py
@@ -1,10 +1,18 @@
 #Escribe un programa de nombre Entrada_conversiones_ejercicio.py que realice lo siguiente:
 
+'''
+Nombre:Rosalinda Aquino Pérez.
+Fecha:21 de Octubre de 2024.
+Descripción: Conversión de cadenas a int, float y boolean mediante la interacción con consola.
+'''
 #a) Pida los datos de los profesores utilizando nombres de variables adecuadas, la función input y el casting:
-
 #Nombre (cadena).
-#No. de cubículo (int).
-#Horas de que imparte clase a la semana (float con 3 decimales).
-#¿Tiene más de 5 años en la unsij? (booleno).
+print("***   Datos de los profesores   ***")
+nombre = input("Ingresa el nombre: ")
+Num_cubo = int(input("Ingrese el No. de cubículo: "))
+H_clase = float(input("Ingrese las horas que imparte clase a la semana:  "))
+tiempo_en_la_unsij = input("¿Tiene más dde 5 años en la unsij (Si/No)?: ")
 #b) Muestre los datos en consola de forma adecuada.
-#Nota: Asuma que el usuario siempre va a ingresar números cuando se requiera
+print()
+print(f"El profesor {nombre}, tiene como número de cubículo: {Num_cubo}, las horas que imparte clase a la semana son: {H_clase:.3f}. Además, tiene más de 5 años en la unsij: {tiempo_en_la_unsij}.")
+#Nota: Asuma que el usuario siempre va a ingresar números cuando se requiera.
