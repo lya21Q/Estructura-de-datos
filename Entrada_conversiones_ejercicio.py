b474aa8bbb520d3bbc9467493a49daa3ab9de3f7
Admin <perezvelayuo@gmail.com>
2024-10-22 20:22:33 -0700

    se realizaron comentarios

---
+++ Entrada_conversiones_ejercicio.py
@@ -0,0 +1,10 @@
+#Escribe un programa de nombre Entrada_conversiones_ejercicio.py que realice lo siguiente:
+
+#a) Pida los datos de los profesores utilizando nombres de variables adecuadas, la función input y el casting:
+
+#Nombre (cadena).
+#No. de cubículo (int).
+#Horas de que imparte clase a la semana (float con 3 decimales).
+#¿Tiene más de 5 años en la unsij? (booleno).
+#b) Muestre los datos en consola de forma adecuada.
+#Nota: Asuma que el usuario siempre va a ingresar números cuando se requiera
