3360c9f4dc8861305a3b5ba870867d9c7023526d
Admin <perezvelayuo@gmail.com>
2024-10-29 01:08:04 -0700

    se realizaron comentarios

--- fundamentos-Python/Sentencias_ejercicio1.py
+++ fundamentos-Python/Sentencias_ejercicio1.py
@@ -3,15 +3,12 @@ Nombre:Rosalinda Aquino Pérez
 Fecha: 28/Octubre/2024
 Descripción: Uso de la sentencia de control if - elif - else.
 '''
-
 print("Este programa determinará cuál de dos números ingresados por el usuario es menor o si son iguales.")
-
 """
 a) Solicite al usuario dos números decimales.
 """
 num1=float(input("Ingrese un número decimal: "))
 num2=float(input("ingrese un segundo número decimal"))
-
 """
 b) Utilice la lógica adecuada para determinar cuál de los dos números es menor o si es que son iguales.
 """
