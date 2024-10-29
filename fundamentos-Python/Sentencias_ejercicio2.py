1ac752b84f396366c8e520653ac3013c92ee6c4b
Admin <perezvelayuo@gmail.com>
2024-10-29 01:25:00 -0700

    se realizaron comentarios

--- fundamentos-Python/Sentencias_ejercicio2.py
+++ fundamentos-Python/Sentencias_ejercicio2.py
@@ -1,24 +1,34 @@
-"""
-Escribe un programa de nombre Sentencias_ejercicio2.py que realice lo siguiente:
-
-Este programa determinará la estación del año de acuerdo al número de mes ingresado por el usuario. Para ello:
+'''
+Nombre:Rosalinda Aquino Pérez
+Fecha: 28/Octubre/2024
+Descripción: Uso de la sentencia de control if - elif - else.
+'''
+print("Este programa determinará la estación del año de acuerdo al número de mes ingresado por el usuario.")
+'''
 
 a) Solicite al usuario un número que representa al mes.
+'''
+mes=int(input("Ingrese un número que represente al mes: "))
 
+'''
 b) Utilice la lógica adecuada para determinar la estación del año de acuerdo con:
-
     3, 4 y 5: Primavera.
-
     6, 7 y 8: Verano.
-
     9,10 y 11: Otoño.
-
     12, 1 y 2: Invierno.
-
     Otro caso: Mensaje de mes incorrecto.
-
+'''
+if mes >= 3 and mes <=5  :
+    estacion = "Primavera"
+elif mes >=6 and mes <=8 :
+    estacion = "Verano"
+elif mes >=9 and mes  <= 11:
+    estacion = "Otoño"
+elif mes ==12 or mes == 1 or mes ==2  :
+    estacion = "Invierno"
+else:
+    print("Mes incorrecto")
+'''
 c) Muestre la estación correspondiente en consola.
-
-
-Observe la salida de la consola como guía:
-"""
+'''
+print(f"La estacion del mes es : {estacion}")
