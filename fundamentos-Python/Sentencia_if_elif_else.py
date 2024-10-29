4c080c9f2c461704dd698bab5320306b5aca9da1
Admin <perezvelayuo@gmail.com>
2024-10-28 21:58:59 -0700

    se realizaron comentarios

--- fundamentos-Python/Sentencia_if_elif_else.py
+++ fundamentos-Python/Sentencia_if_elif_else.py
@@ -1,20 +1,15 @@
 '''
-Nombre:
-Fecha:
-Descripción:
-Uso de la sentencia de control if - elif - else.
+Nombre:Rosalinda Aquino Pérez
+Fecha: 28/Octubre/2024
+Descripción: Uso de la sentencia de control if - elif - else.
 '''
-
 """
 La sentencia elif es una extensión del if-else y permite evaluar múltiples condiciones de forma secuencial. 
 Es como tener varias opciones a elegir, ejecutándose el bloque de código correspondiente a la primera condición 
 que sea verdadera.
-
 Sintaxis:
-
 if condicion_1:
     # Código a ejecutar si la condición_1 es verdadera.
-
 elif condición_2:
     # Código a ejecutar si la condición_2 es verdadera.
   .
@@ -24,35 +19,24 @@ else:
     # Código que se ejecuta si todas las condiciones fueron falsas.
 """
 
-# Ejemplo que determina el grupo al que pertenece de acuerdo a su edad.
-print("  ***  Programa que determina el grupo de acuerdo a la edad  ***")
-edad = int(input("Ingresa tu edad: "))
+print("  ***  Programa que determina el grupo de acuerdo a la edad  ***")#Imprime un mensaje en pantalla.
+edad = int(input("Ingresa tu edad: "))#Solicita su edad al usuario.
 
-# lógica para determinar el grupo usando la estrucutra if-elif-else
-if edad < 14:
+# ocupamos la condicional if-elif-else para determinar a que grupo pertenece de acuerdo a la edad ingresada.
+if edad < 14:#Si la edad es menor que 14, se asigna el valor "Niños y adolescentes"
     grupo = "Niños y adolescentes"
 
-elif 15 <= edad < 25:
+elif 15 <= edad < 25:#Si la edad es menor o igual a 14, y menor a 25 se asigna el valor "Jóvenes" a ala variable grupo.
     grupo = "Jóvenes"
 
-elif 25 <= edad < 45:
+elif 25 <= edad < 45:#Si la edad es menor o igual a 25, y menor a 45 se asigna el valor "Adultos jóvenes" a ala variable grupo.
     grupo = "Adultos jóvenes"
 
-elif 45 <= edad < 60:
+elif 45 <= edad < 60:#Si la edad es menor o igual a 45, y mayor a 60 se asigna el valor "Adultos Maduros" a ala variable grupo.
     grupo = "Adultos maduros"
 
 else:
-    grupo = "Adultos mayores"
+    grupo = "Adultos mayores" #Si ninguna de las condiciones fue verdadera, entonces se le asigna el valor de "Adultos mayores", a la variable grupo.
 
-print(f"El grupo al que perteneces es: {grupo}.")
+print(f"El grupo al que perteneces es: {grupo}.")#Se imprime el resultado obtenido.
 
-###############################################
-"""
-MODO DEPURACIÓN (DEBUG)
-
-Utilizar ahora el modo depuración.
-1) Crear un punto de ruptura en la variable edad. Marcar el número de línea y se pondrá un círculo color rojo.
-2) Clic derecho y ejecutar el modo Debug.
-3) Observar la nueva pantalla e ir ejecutando paso-a-paso (step over) F8.
-4) Observar el comportamiento.
-"""
