092f003fa98f24b364ffec8ecedef7faf98d8b19
Admin <perezvelayuo@gmail.com>
2024-10-29 01:26:04 -0700

    se realizaron comentarios

--- fundamentos-Python/Sentencias_ejercicio1.py
+++ fundamentos-Python/Sentencias_ejercicio1.py
@@ -3,22 +3,17 @@ Nombre:Rosalinda Aquino Pérez
 Fecha: 28/Octubre/2024
 Descripción: Uso de la sentencia de control if - elif - else.
 '''
+
 print("Este programa determinará cuál de dos números ingresados por el usuario es menor o si son iguales.")
-"""
-a) Solicite al usuario dos números decimales.
-"""
+
 num1=float(input("Ingrese un número decimal: "))
 num2=float(input("ingrese un segundo número decimal"))
-"""
-b) Utilice la lógica adecuada para determinar cuál de los dos números es menor o si es que son iguales.
-"""
+
 if num1 < num2 :
     print("El número uno es menor.")
 elif num1 > num2 :
     print("El número dos es menor.")
 else :
     print("Son iguales.")
-"""
-c) Muestre el número menor (o que son iguales) en consola.
-"""
+
 print(f"El numero {num1} es menor que {num2}")
--- fundamentos-Python/Sentencias_ejercicio2.py
+++ fundamentos-Python/Sentencias_ejercicio2.py
@@ -4,20 +4,7 @@ Fecha: 28/Octubre/2024
 Descripción: Uso de la sentencia de control if - elif - else.
 '''
 print("Este programa determinará la estación del año de acuerdo al número de mes ingresado por el usuario.")
-'''
-
-a) Solicite al usuario un número que representa al mes.
-'''
 mes=int(input("Ingrese un número que represente al mes: "))
-
-'''
-b) Utilice la lógica adecuada para determinar la estación del año de acuerdo con:
-    3, 4 y 5: Primavera.
-    6, 7 y 8: Verano.
-    9,10 y 11: Otoño.
-    12, 1 y 2: Invierno.
-    Otro caso: Mensaje de mes incorrecto.
-'''
 if mes >= 3 and mes <=5  :
     estacion = "Primavera"
 elif mes >=6 and mes <=8 :
@@ -28,7 +15,4 @@ elif mes ==12 or mes == 1 or mes ==2  :
     estacion = "Invierno"
 else:
     print("Mes incorrecto")
-'''
-c) Muestre la estación correspondiente en consola.
-'''
 print(f"La estacion del mes es : {estacion}")
