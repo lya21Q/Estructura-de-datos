144e9a55a95d5bcfb1dbd7a0d07a102f64d4dc0e
Admin <perezvelayuo@gmail.com>
2024-10-29 00:27:51 -0700

    se realizaron comentarios

--- fundamentos-Python/Operadores_ejercicio3.py
+++ fundamentos-Python/Operadores_ejercicio3.py
@@ -6,24 +6,23 @@ Este programa determinará si un usuario se autentifica correctamente con su usu
 """
 a) Internamente declare dos cadenas constantes, una para el usuario y otra para la contraseña.
 """
-
-usuario=string(usuario)
-contrasena= string(contrasena)
+usuario1= "usuario"
+contrasena1= "contrasena"
 """
 b) Pida al usuario una cadena con el usuario.
 """
-
+usuario = input("Ingrese el nombre de usuario: ")
 
 """
 c) Pida al usuario una cadena con la contraseña.
 """
-
-
+contrasena = input("Ingrese la contraseña: ")
 """
 d) Si ambas cadenas son iguales a las cadenas declaradas internamente, entonces el usario se autenticó correctamente.
 """
-
+variable = (usuario == usuario1) and (contrasena == contrasena1)
 
 """
 e) Muestre el resultado en consola como valor booleano (True/False).
 """
+print(f"La revisión fue correcta: {variable}")
