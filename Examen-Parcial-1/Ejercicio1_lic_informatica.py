b60a12537fa8c5a9b69a714125b3b2ea28aa9306
rosalinda <perezvelayuo@gmail.com>
2024-10-31 16:09:45 -0600

    como hacer

--- Examen-Parcial-1/Ejercicio1_lic_informatica.py
+++ Examen-Parcial-1/Ejercicio1_lic_informatica.py
@@ -2,15 +2,19 @@
 # del 1 hasta un número ingresado por el usuariio.
 
 
-cantidad=int(input(("Ingrese un número:")))
-print()
 
-cont=0
+contador=0
+
 numero=int(input(("Ingrese un número:")))
-while numero <= cantidad :
-    if numero % 3==0 :
-        print(numero)
-    elif :
+while contador <= numero:
+    if contador % 3==0 :
+        print("Licenciatura")
+        print(contador, end=" ")
+    elif contador % 5 == 0 :
+        print("Informatimática")
+    elif contador % 3== 0 and contador % 5 == 0:
+        print("Licenciatura en informatica")
         print()
-elif :
-    print()
+    else :
+        print(contador)
+    contador += 1
