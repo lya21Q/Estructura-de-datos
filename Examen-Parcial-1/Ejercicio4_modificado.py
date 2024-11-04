d9751a1df8f5ade8730dcb9aaf934788a7313e02
rosalinda <perezvelayuo@gmail.com>
2024-11-04 17:34:31 -0600

    como hacer

---
+++ Examen-Parcial-1/Ejercicio4_modificado.py
@@ -0,0 +1,63 @@
+from random import randint
+#Rosalinda Aquino Pérez
+#03 Noviembre 2024
+#a) El número a adivinar es un número aleatorio entre 1 y 100.
+n_adivinar=0
+intentos =0
+numero=0
+
+var=input("¿En que dificultad deseas jugar: (dificil, facil, medio)?")
+var=var.lower()
+if var = "dificil" :
+    intentos = 10
+if var = "medio" :
+        intentos =5
+
+
+
+while numero >= intentos:
+    if var == "dificil" :
+        n_adivinar = randint(1, 110)
+        intentos=10
+        print(f"Numero de intento: {intentos}.", end=" ")
+        numero = int(input("Ingresa un número entre : (1-100): "))
+        if numero >= n_adivinar :
+            print("El número es mayor")
+        elif numero <= n_adivinar :
+            print("El numero a adivinar es mayor")
+        else :
+            print(f"¡Felicidades! adivinate el número en : {intentos} intentos ")
+            break
+    elif var =="medio" :
+        n_adivinar = randint(1, 100)
+        intentos = 5
+        print(f"Numero de intento: {intentos}.", end=" ")
+        numero = int(input("Ingresa un número entre : (1-100): "))
+        if numero >= n_adivinar :
+            print("El número es mayor")
+        elif numero <= n_adivinar :
+            print("El numero a adivinar es mayor")
+        else :
+            print(f"¡Felicidades! adivinate el número en : {intentos} intentos ")
+            break
+    elif var== "facil" :
+        n_adivinar = randint(1, 50)
+        intentos = 4
+        print(f"Numero de intento: {intentos}.", end=" ")
+        numero = int(input("Ingresa un número entre : (1-100): "))
+        if numero >= n_adivinar:
+            print("El número es mayor")
+        elif numero <= n_adivinar:
+            print("El numero a adivinar es mayor")
+        else:
+            print(f"¡Felicidades! adivinate el número en : {intentos} intentos ")
+            break
+    if numero!= n_adivinar :
+        print(f"lo sentimos el numero a adivinar era : {n_adivinar}")
+
+#e) Si el jugador no adivina el número, el juego muestra un mensaje con el número.
+
+#solicitar inicialmente la dificultad
+#f 10intentos y números 1-50
+#m lo que tiene
+#D 4 intentos numeros entre el 1-110
