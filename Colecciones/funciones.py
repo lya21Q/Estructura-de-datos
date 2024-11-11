f173da3e4ab7066ffb418746a1c5536505a589a9
rosalinda <perezvelayuo@gmail.com>
2024-11-11 15:57:31 -0600

    como utilizar Ciclo for

---
+++ Colecciones/funciones.py
@@ -0,0 +1,12 @@
+nombre=input("ingresa nombre: ")
+nombre=nombre.lower
+
+def hola(nombre) :
+    print(f"Hola {nombre}")
+print("Adios")
+
+def menu(menú    :)
+
+
+def calculadora(opcion,num1,num2) :
+    op=int(input("Ingrese la opción : "))
