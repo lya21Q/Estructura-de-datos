b12027af87639e77aa629996c42e61b258cba3c9
rosalinda <perezvelayuo@gmail.com>
2024-11-12 16:24:06 -0600

    como utilizar Ciclo for

---
+++ Funciones-y-colecciones/listas.py
@@ -0,0 +1,18 @@
+#crear una lista
+#Alumnos =[]
+from os import remove
+
+alumnos=["Addi","Jesus Alberto","Juan"]
+alumnos.append("Hector")
+print(alumnos)
+print(alumnos[1])
+alumnos.insert(0,"Tania")
+print(alumnos)
+
+for alumno in alumnos :
+    print(alumno,end=" ")
+    #remove("Hector")#Eliminar por valor
+    for alumno in alumnos :
+        print(alumno)
+    #delet=(2)
+print()
---
+++ Funciones-y-colecciones/listas_1.py
@@ -0,0 +1,25 @@
+alumnos=[]
+alumnos.append("Addi")
+alumnos.append("Alberto")
+alumnos.append("Bryan")
+alumnos.append("Gali")
+alumnos.append("Hector")
+alumnos.append("Jennifer")
+alumnos.append("Juan")
+alumnos.append("Patty")
+alumnos.append("Rebeca")
+alumnos.append("Rosalinda")
+alumnos.append("Tania")
+
+
+len(alumnos)
+print(alumnos)
+print()
+
+alumnos.sort()
+print(alumnos)
+print()
+
+alumnos.sort(reverse=True)
+print (alumnos)
+print()
