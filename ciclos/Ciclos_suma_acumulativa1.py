7bab33a41e15363d43cd3559d0e934a9b97bba62
Rosalinda <aquinorosalinda@gmail.com>
2024-11-26 23:20:57 -0600

    comentarios

--- ciclos/Ciclos_suma_acumulativa1.py
+++ ciclos/Ciclos_suma_acumulativa1.py
@@ -1,16 +1,15 @@
 """
 Ejercicio 1: Suma acumulativa.
 Nombre : Rosalinda Aquino Pérez
-
 """
 print("Programa que cálcula la suma acumulativa")
-numero_f=int (input("Ingrese el número final: "))
+numero_f=int (input("Ingrese el número final: "))#Solicitamos al usuario que ingrese el número final hasta el cual desea calcular la suma.
 print(f"La suma del 0 hasta el {numero_f}, es : ")
-contador = 0
-aux = 0 #Inicializamos el contador en cero.
+contador = 0#Inicializamos el contador en cero, el contador llevará un conteo de cero, hasta el número final que se ingrese.
+aux = 0# inicializamos aux en cero, que llevara la suma acumulada de los valores del contador.
 
-while contador <= numero_f :#
-    aux += contador
+while contador <= numero_f :#Este bucle se ejecutara mientras contador sea menor o igual a numero final.
+    aux += contador#sumamos el valor actual de contador a la variable aux.
     contador +=1
-print(aux)
+print(aux)#imprimimos el valor que contiene la variable aux.
 
