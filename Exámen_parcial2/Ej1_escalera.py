054ea85d8ca2fc0f7de84c73140becefddd8a04f
Rosalinda <aquinorosalinda@gmail.com>
2024-12-09 00:23:29 -0600

    elaboracion de una piramide de asteriscos

--- Exámen_parcial2/Ej1_escalera.py
+++ Exámen_parcial2/Ej1_escalera.py
@@ -7,20 +7,20 @@ Si el número es negativo, la escalera será descendente.
 """
 
 numero_escalones=None
-espacio=" "
-escalera=None
-def escalera_ascendente(numero_escalones,espacio,escalera):
+
+def escalera_ascendente(numero_escalones):
     while numero_escalones != 0:
         print("***  Ejercicio 1. La escalera.  ***")
         numero_escalones=int(input("Ingresa el número de escalones (positivo - ascendente y negativo - descendente) o ingresa un cero para salir:"))
-        if numero_escalones>0:
+        if numero_escalones > 0:
             for i in range(numero_escalones):
-                escalera= " " * i + "_|"
+                espacio = " " * (numero_escalones - i - 1)
+                escalera= espacio + "_|"
                 print(f"{escalera}")
         elif numero_escalones < 0:
-            for i in range(-1,numero_escalones):
-                espacio = numero_escalones - i -1
-                escalera=" " * espacio + "_|"
+            for i in range(-numero_escalones):
+                espacio = i
+                escalera= " " * espacio + "_|"
                 print(f"{escalera}")
         elif numero_escalones == 0:
             print("Saliendo del programa.")
@@ -28,4 +28,4 @@ def escalera_ascendente(numero_escalones,espacio,escalera):
             print("Opción no válida.")
     return numero_escalones
 
-numero_escalones=escalera_ascendente(numero_escalones,espacio,escalera)
+numero_escalones=escalera_ascendente(numero_escalones)
