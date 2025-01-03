36e3d3eab7d55c463b6b32951cd1dcf1b1f475b6
Rosalinda <aquinorosalinda@gmail.com>
2025-01-02 20:06:18 -0600

    se corrigio y se agregaron comentarios

--- Funciones-y-colecciones/Listas_ej2_compras.py
+++ Funciones-y-colecciones/Listas_ej2_compras.py
@@ -12,40 +12,38 @@ Ejemplos de uso de las Tuplas.
 #3)eliminar producto
 #4)salir
 
-op= None
-productos = []
-cantidades = []
-#producto=(nombre,cantidad)
-#lista_productos.append(producto)
-
+op= None#Variable que controla las opciones.
+productos = []#LLista 1
+cantidades = []#Lista 2
 while op!=4:
+    #Muestra un menú al usuario.
     print("* Lista de compras *")
     print("[1].-Ver lista ")
     print("[2].-Añadir producto a la lista ")
     print("[3].-Eliminar producto de la lista ")
     print("[4].-Salir ")
     op = int(input("Ingrese opción:"))
-
+    #Caso para ver la lista de productos.
     if op==1:
         indice = 0
         contador=0
         for producto in productos :
-            print((f"las cantidades {cantidades[contador]} y los productos es: {producto} "))
+            print((f"la cantidad es: {cantidades[contador]}, y los productos es: {producto}. "))
             contador+=1
+    #Caso para añadir productos a la lista.
     elif op==2:
         nombre = input("ingrese el nombre del producto:")
         cantidad = int(input("Ingrese la cantidad que desea añadir:"))
         productos.append(nombre)
         cantidades.append(cantidad)
+    #Caso para eliminar productos a la lista.
     elif op==3:
         nombre_producto=input("Ingrese el nombre del producto que desea eliminar:")
         numero_producto=0
-
-        while producto[contador]!=nombre_producto:
-            numero_producto+=0
-
-            productos.remove(nombre_producto)
-            del  cantidades[numero_producto]
+        while productos[numero_producto]!=nombre_producto:
+            cantidades+= 1
+        productos.remove(nombre_producto)
+        del cantidades[numero_producto]
         print("Producto eliminado")
     elif op==4:
         print("Saliendo del programa")
