3043ae59c29715bbf85bcc7a422c4fff62c2bfe9
Rosalinda <aquinorosalinda@gmail.com>
2025-01-06 00:12:53 -0600

    se corrigio y se agregaron comentarios

--- Funciones-y-colecciones/Listas_ej3_calificaciones.py
+++ Funciones-y-colecciones/Listas_ej3_calificaciones.py
@@ -1,25 +1,79 @@
 '''
 Nombre:Rosalinda AAquino Pérez
 Fecha:21/11/2024
-Descripción:
-Ejemplos de uso de las Tuplas.
+Descripción:Este programa es una lista de las calificaciones de los alumnos del Parcial 1. La lista está conformada por el nombre del alumno y sus calificaciones.
+Cada alumno tiene 5 calificaciones: estructuras de datos, derecho, contabilidad, álgebra y electrónica.
+Se debe mostrar el siguiente menú:
+  *** Calificaciones del Parcial 1 ***
+1) Ver calificaciones de todos los alumnos.
+2) Ver calificaciones detalladas de un alumno.
+3) Ver promedios del Parcial 1 de todos los alumnos.
+4) Ver promedio grupal del Parcial 1.
+5) Agregar alumno y sus calificaciones.
+6) Eliminar alumno y sus calificaciones.
+0) Salir.
+Cualquier otro caso -> Opción no válida.
 '''
-"""
-Claificaciones del parcial
-"""
-from promedio import calificaciones
+op=None
+def menu():
+    print("[1].-Ver calificaciones de todos los alumnos.")
+    print("[2].-Ver calificaciones detalladas de un alumno.")
+    print("[3].-Ver promedios del Parcial 1 de todos los alumnos.")
+    print("[4].- Ver promedio grupal del Parcial 1.")
+    print("[5].-Agregar alumno y sus calificaciones.")
+    print("[6].-Eliminar alumno y sus calificaciones.")
+    print("[0].- Salir.")
+    op=int(input("Ingrese una opción:"))
+    return op
 
-primer_parcial=float(input("ingrese la caficación parcial"))
-primer_segundo=float(input("ingrese la caficación ordinaria"))
-primer_tercero=float(input("ingrese la caficación final"))
-primer_ordinario=float(input("ingrese la caficación final"))
+alumnos=[]
+estructura_de_datos=[]
+derecho=[]
+contabilidad=[]
+algebra=[]
+electronica=[]
+calificacion=None
+while op!=0:
+    op = menu()
+    #Caso para salir del programa.
+    if op==0:
+        print("Saliendo del programa...")
+    #Caso para ver calificaciones de todos los alumnos.
+    elif op==1:
+        print("Las calificaciones de todos los alumnos son:")
+        for alumno in range (len(alumnos)):
+            print(f"El alumno:{alumnos}, calificaciones: {estructura_de_datos},{derecho},{contabilidad},{algebra},{electronica}")
+    #Caso para ver calificaciones detalladas de un alumno.
+    elif op==2:
+        alumno_1=input("Ingresa el nombre del alumno:")
+        for alumno in alumnos:
+            if alumno_1==alumnos:
+                print(f"Calificaciones del alumno:{alumno_1}{estructura_de_datos}{derecho}{contabilidad}{algebra}{electronica}")
+    #Caso para ver promedios del parcial 1 de todos los alumnos.
+    elif op==3:
+        print()
+    #Caso para ver promedio grupal del parcial 1.
+    elif op==4:
+        if len(alumnos)>0:
+            total_promedio=sum(estructura_de_datos+derecho+contabilidad+algebra+electronica)
+            print(f"Promedio grupal del parcial 1 es: {total_promedio}")
+    #Caso pra agregar alumno y sus calificaciones.
+    elif op==5:
+        alumno=input("Ingrese el nombre del alumno:")
+        cal_estructura=float(input("Ingresa la calificacion de estructura de datos: "))
+        cal_derecho = float(input("Ingresa la calificacion de derecho: "))
+        cal_contabilidad = float(input("Ingresa la calificacion de contabiliidad: "))
+        cal_algebra = float(input("Ingresa la calificacion de algebrà: "))
+        cal_electronica = float(input("Ingresa la calificacion de electronica: "))
 
+        alumnos.append(alumno)
+        estructura_de_datos.append(cal_estructura)
+        derecho.append(cal_derecho)
+        contabilidad.append(cal_contabilidad)
+        algebra.append(cal_algebra)
+        electronica.append(cal_electronica)
+    #Caso para eliminar alumno y sus calificaciones.
+    elif op==6:
+        print()
 
-def calificacion(calificaciones):
-    sum=(calificaciones[0:2]/3)
-    promedio_parcial=sum(calificaciones[3]/2)
-    promedio_final=(promedio_parcial+calificaciones[3]/2)
-    tupla_promedios=(promedio_parcial,promedio_final)
-    return promedio_parcial,promedio_final
-
-promedio_parcial,promedio_final=calificacion(calificaciones)
+op=menu()
--- Funciones-y-colecciones/Tuplas_ej2_coordenadas.py
+++ Funciones-y-colecciones/Tuplas_ej2_coordenadas.py
@@ -4,47 +4,58 @@ Fecha:21/11/2024
 Descripción:
 Ejercicio 1.
 """
+op=None
+#Muestra el menú al usuario.
 print("Este programa almacena diversos puntos como coordenadas y permite obtener la ecuación de la recta entre dos de los puntos.")
 def menu():
     print("[1] Ver cordenadas")
     print("[2] Agregar coordenada")
     print("[3] Calcular la pendiente y la ecuación de la recta entre dos corrdenadas.")
-    print("[4] Eliminar coordenada]")
+    print("[4] Eliminar coordenada")
     print("[0] Salir")
+    #Solicita al usuario que ingrese una opción.
     op=int(input("Ingrese una opcion: "))
     return op
+
 coordenada_X=0
 coordenada_Y=0
-tupla=(coordenada_X, coordenada_Y)
-lista=[tupla]
-def coordenadas(op,tupla,lista):
-    if op == 1:
-        if not tupla:
-            print(f"No hay coordenadas para mostrar")
-            print()
-            print("------------------------------------")
+tupla=()
+coordenadas=[]
+
+while op!=0:
+    op=menu()
+    if op==0:
+        print("Saliendo del programa.")
+    elif op == 1:
+        if len(coordenadas)!=0:
+            print(f"La lista de números es: {coordenadas}")
         else:
-            for f in lista:
-                print("Lista de cooordenadas.")
-                print()
-                print(f"Cordenada{f+1}: {tupla}")
+            print("No hay valores que mostrar.")
     elif op == 2:
         coordenada_X=float(input("Ingrese coordenada X"))
         coordenada_Y=float(input("Ingrese coordenada Y"))
-        lista.append((coordenada_X, coordenada_Y))
-        tupla=(coordenada_X, coordenada_Y)
+        tupla=(coordenada_X,coordenada_Y)
+        coordenadas.append((tupla))
         print(f"La coordenada {coordenada_X,coordenada_Y} se agrego con éxito!. ")
     elif op == 3:
-        if not tupla:
-            print(f"No hay coordenadas para mostrar")
-            print()
-            print("-------------------------------------")
+        if len(coordenadas)!=0:
+            print("No se puede calcular la pendiente.")
         else:
-            print("Lista de cooordenadas.")
-    elif op == 0:
-        print()
+            numero=+1
+            indice1_=int(input("Ingrese el indice de la primera coordenada:"))
+            indice2_=int(input("Ingrese el indice de la segunda coordenada:"))
+            coordenada_x1,coordenada_Y1=(coordenadas[indice1_])
+            coordenada_X2,coordenada_Y2=(coordenadas[indice2_])
+            #Pendiente : y=mx+b
+            m=(coordenada_Y2-coordenada_Y1)/(coordenada_X2-coordenada_x1)
+            b=coordenada_Y1-m*coordenada_x1
+            print(f"La pendiente es {m}")
+            print(f"Expresión de la recta es: y={m}x+({b}")
+    elif op == 4:
+        if len(coordenadas)!=0:
+            eliminar=int(input("Ingrese el indice de la coordenada que quiera eliminar."))
+            del coordenadas[eliminar]
+        else:
+            print("No hay coordenadas para eliminar.")
     else:
-        print("Saliendo del programa")
-    return tupla
-op=menu()
-tupla=coordenadas(op,tupla,lista)
+        print(" Opción incorrecta.")
