ed6e05dc8608482a514f1bd1e87f67315b09f53e
Rosalinda <aquinorosalinda@gmail.com>
2025-01-08 20:37:36 -0600

    se corrigio y se agregaron comentarios

--- Nuevas practicas/marcadores_de_posicion.py
+++ Nuevas practicas/marcadores_de_posicion.py
@@ -12,7 +12,7 @@ from paramiko.common import ERROR
 opcion=None
 #TODO implementar menu
 def menu():
-    pass #
+    pass #La función pass indica que no hace nada por el momento.
 #TODO implementar cadena a entero.
 def cadena_a_entero(cadena):
     ...
@@ -26,11 +26,7 @@ def cadena_a_flotante(cadena):
             num_cadena=input("Ingresa un número a convertir")
             num=cadena_a_entero(num_cadena)
         elif opcion ==2:
-            num_cadena_1=input("Ingresa un mumero a convertir.")
-            num_cadena_1(num_cadena_1)
+            num_cadena=input("Ingresa un mumero a convertir.")
+            num_cadena=cadena_a_flotante(num_cadena)
         elif opcion==0:
             print("Salir.")
-
-cadena=cadena_a_entero(cadena)
-cadena=cadena_a_flotante(cadena)
-opcion=menu()
